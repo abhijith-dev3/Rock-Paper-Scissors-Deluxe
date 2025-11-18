542519b1d78a83f5d7a09d93f06ebed17d2af143
Abhijith S <abhijith4389@gmail.com>
2025-11-18 16:18:00 +0530

    updated

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function getRoundResults(userOption) {
     playerScore++;
     return `Player wins! ${userOption} beats ${computerResult}`;
   } else if (computerResult === userOption) {
-    return `It's a tie! Both chose ${userOption}`;
+    return `It's a tie! Both choose ${userOption}`;
   } else {
     computerScore++;
     return `Computer wins! ${computerResult} beats ${userOption}`;
